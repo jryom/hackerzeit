7832bcaf3466c50cbc0355583e0efafa5e8f602e
Jesper Ryom <jesper@ryom.me>
2020-04-20 13:37:55 +0200

    feat: add basic theme and styles

---
+++ src/styles/global.js
@@ -0,0 +1,49 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+ul[class],
+ol[class] {
+  padding: 0;
+  list-style: none;
+}
+
+body,
+h1,
+h2,
+h3,
+h4,
+p,
+ul[class],
+ol[class],
+li,
+figure,
+figcaption,
+blockquote,
+dl,
+dd {
+  margin: 0;
+}
+
+body {
+  min-height: 100vh;
+  scroll-behavior: smooth;
+  text-rendering: optimizeSpeed;
+  line-height: 1.5;
+}
+
+a:not([class]) {
+  text-decoration-skip-ink: auto;
+}
+
+input,
+button,
+textarea,
+select {
+  font: inherit;
+}`;
