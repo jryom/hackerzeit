cd86ef6f9249325941225d776730ebfae208d471
Jesper Ryom <jesper@ryom.me>
2020-04-23 21:13:12 +0200

    style: dark mode for loading indicator

--- src/components/loading-indicator.js
+++ src/components/loading-indicator.js
@@ -1,7 +1,7 @@
 import styled, { keyframes } from 'styled-components';
 import { space } from 'styled-system';
 
-const pulsate = keyframes`
+const darkPulsate = keyframes`
   from {
     background: rgba(0,0,0,0.45);
   }
@@ -11,9 +11,22 @@ const pulsate = keyframes`
   }
 `;
 
+const lightPulsate = keyframes`
+  from {
+    background: rgba(255,255,255,0.45);
+  }
+
+  to {
+    background: rgba(255,255,255,0.05);
+  }
+`;
+
 export default styled.div`
-  animation: ${pulsate} 0.3s 0.1s infinite alternate;
-  background: rgba(0, 0, 0, 0.45);
+  animation-delay: 0.1s;
+  animation-direction: alternate;
+  animation-duration: 0.3s;
+  animation-iteration-count: infinite;
+  animation-name: ${darkPulsate};
   height: 8px;
   margin: 0 auto;
   position: relative;
@@ -21,24 +34,35 @@ export default styled.div`
 
   &:after,
   &:before {
-    background: rgba(0, 0, 0, 0.45);
+    animation-direction: alternate;
+    animation-duration: 0.3s;
+    animation-iteration-count: infinite;
+    animation-name: ${darkPulsate};
     content: '';
     display: block;
     height: 8px;
+    left: 0;
     position: absolute;
     top: 0;
-    left: 0;
     width: 8px;
   }
 
   &:before {
-    animation: ${pulsate} 0.3s infinite alternate;
     transform: translateX(-166%);
   }
 
   &:after {
-    animation: ${pulsate} 0.4s 0.2s infinite alternate;
+    animation-delay: 0.2s;
     transform: translateX(166%);
+  }
+
+  @media (prefers-color-scheme: dark) {
+    animation-name: ${lightPulsate};
+
+    &:after,
+    &:before {
+      animation-name: ${lightPulsate};
+    }
   }
 
   ${space}
