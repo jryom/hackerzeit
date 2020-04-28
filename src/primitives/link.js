1ef552d786de9b5ad9a5a36dacd1644c9e804daf
Jesper Ryom <jesper@ryom.me>
2020-04-28 09:42:47 +0200

    feat: add relative and absolute url support to link primitive

--- src/primitives/link.js
+++ src/primitives/link.js
@@ -1,5 +1,32 @@
+import Link from 'next/link';
 import styled from 'styled-components';
 
-export default styled.a`
+const isRelativeUrl = (url) => {
+  const link = document.createElement('a');
+  link.href = url;
+  return link.origin + link.pathname + link.search + link.hash === url;
+};
+
+const Anchor = styled.a`
+  cursor: pointer;
   text-decoration: none;
 `;
+
+const StyledLink = ({ children, href }) => {
+  if (isRelativeUrl(href)) {
+    return (
+      <Link href={href}>
+        <Anchor>{children}</Anchor>
+      </Link>
+    );
+  }
+
+  return <Anchor href={href}>{children}</Anchor>;
+};
+
+StyledLink.propTypes = {
+  children: PropTypes.node.isRequired,
+  href: PropTypes.string.isRequired,
+};
+
+export default StyledLink;
