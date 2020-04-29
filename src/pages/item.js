7de2983d8f3a940f8344a6165714125cef3f3ced
Jesper Ryom <jesper@ryom.me>
2020-04-29 13:02:22 +0200

    fix: remove redundant Link component

--- src/pages/item.js
+++ src/pages/item.js
@@ -2,22 +2,20 @@ import { useRouter } from 'next/router';
 import useSWR from 'swr';
 
 import { ItemSubtitle, ItemTitle, LoadingIndicator } from '@/components';
-import { Box, Link } from '@/primitives';
+import { Box } from '@/primitives';
 import { fetch } from '@/utils';
 
 const Item = () => {
   const router = useRouter();
   const { id } = router.query;
-  const { data, error } = useSWR(`/api/item?id=${id}`, fetch);
+  const { data } = useSWR(`/api/item?id=${id}`, fetch);
 
   if (!data) return <LoadingIndicator />;
 
   return (
     <Box>
-      <Link href={data.url}>
-        <ItemTitle data={data} />
-        <ItemSubtitle data={data} />
-      </Link>
+      <ItemTitle data={data} />
+      <ItemSubtitle data={data} />
     </Box>
   );
 };
