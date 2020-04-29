f0edec93b2f1543a9ce8adb4cfc6f62a37b0848e
Jesper Ryom <jesper@ryom.me>
2020-04-29 13:05:56 +0200

    fix: don't reuse intersection observer ran into a react error about updating state on unmounted component when navigating back to the index page from an item page, despite calls to unobserve() or disconnect().

--- src/utils/intersection-observer.js
+++ src/utils/intersection-observer.js
@@ -1,20 +1,10 @@
-let intersectionObserver;
-
 export default (handler) => {
-  if (!intersectionObserver) {
-    if (typeof window !== 'undefined' && !window.IntersectionObserver) {
-      require('intersection-observer'); // eslint-disable-line global-require
-    }
-
-    intersectionObserver = new IntersectionObserver((entries, observer) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          observer.unobserve(entry.target);
-          handler();
-        }
-      });
+  return new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        observer.unobserve(entry.target);
+        handler();
+      }
     });
-  }
-
-  return intersectionObserver;
+  });
 };
