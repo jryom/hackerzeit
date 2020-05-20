f77c15a77bb7a91d4f10a100a4aa5a45d9e377fd
Jesper Ryom <jesper@ryom.me>
2020-05-20 09:47:16 +0200

    style: tweak LoadButton

--- src/components/load-button.js
+++ src/components/load-button.js
@@ -1,9 +1,18 @@
-import { Box } from '@/primitives';
+import { Box, Text } from '@/primitives';
 
 const LoadButton = ({ loadMore }) => {
   return (
-    <Box as="button" onClick={loadMore}>
-      Load more comments
+    <Box
+      as="button"
+      bg="lightGray"
+      border="none"
+      display="block"
+      mx="auto"
+      onClick={loadMore}
+      px={3}
+      py={2}
+    >
+      <Text size="xs">Load more</Text>
     </Box>
   );
 };
