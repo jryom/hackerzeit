27da4ba9fd445134fe376a189551626bae9bc4cc
Jesper Ryom <jesper@ryom.me>
2020-05-26 14:47:50 +0200

    style: add active link style to nav

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,4 +1,5 @@
 import css from '@styled-system/css';
+import { useRouter } from 'next/router';
 import styled from 'styled-components';
 
 import { PAGES } from '@/constants';
@@ -6,24 +7,45 @@ import { Box, Link, Text } from '@/primitives';
 
 const Li = styled.li`
   display: inline;
+  position: relative;
+
   ${css({
-    marginLeft: 2,
+    marginRight: 3,
   })}
+
+  &:after {
+    background: ${({ theme }) => theme.colors.darkGray};
+    bottom: -1px;
+    content: '';
+    display: ${({ active }) => (active ? 'block' : 'none')};
+    height: 2px;
+    left: 0;
+    position: absolute;
+    width: 100%;
+
+    @media (prefers-color-scheme: dark) {
+      color: ${({ theme }) => theme.colors.nearWhite};
+    }
+  }
 `;
 const Ol = styled.ol`
   list-style: none;
 `;
 
-export default () => (
-  <Box bg="accent" maxWidth={8} mx="auto" py={2}>
-    <Ol>
-      {Object.keys(PAGES).map((key) => (
-        <Li key={PAGES[key]}>
-          <Link as={`/${key.toLowerCase()}`} href="/[page]" marginRight={2}>
-            <Text>{PAGES[key]}</Text>
-          </Link>
-        </Li>
-      ))}
-    </Ol>
-  </Box>
-);
+export default () => {
+  const { asPath } = useRouter();
+
+  return (
+    <Box bg="accent" maxWidth={8} mx="auto" px={3} py={3}>
+      <Ol>
+        {Object.keys(PAGES).map((key) => (
+          <Li key={PAGES[key]} active={asPath.includes(key.toLowerCase())}>
+            <Link as={`/${key.toLowerCase()}`} href="/[page]">
+              <Text size="s">{PAGES[key]}</Text>
+            </Link>
+          </Li>
+        ))}
+      </Ol>
+    </Box>
+  );
+};
