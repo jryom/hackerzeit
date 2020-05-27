0de988ee331bcfdd0495b20622c5f7f68448e8e8
Jesper Ryom <jesper@ryom.me>
2020-05-27 20:29:33 +0200

    feat: parse html from comments

--- src/components/recursive-comments.js
+++ src/components/recursive-comments.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import { Box, Text } from '@/primitives';
-import { relativeTime } from '@/utils';
+import { parseComment, relativeTime } from '@/utils';
 
 const RecursiveComments = ({ comment }) => (
   <Box mt={2}>
@@ -10,7 +10,16 @@ const RecursiveComments = ({ comment }) => (
     <Text darkVariant="gray" italic ml={1} size="xs">
       {relativeTime(comment.time)}
     </Text>
-    <Text as="p" dangerouslySetInnerHTML={{ __html: comment.text }} size="xs" />
+    <Text
+      as="div"
+      css={`
+        overflow-wrap: anywhere;
+      `}
+      lineHeight="1.4"
+      size="xs"
+    >
+      {parseComment(comment.text)}
+    </Text>
     <Box
       borderLeft={1}
       borderLeftColor="lightGray"
