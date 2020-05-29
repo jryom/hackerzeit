75cdb42341d4c864a449211f38da0a8a2cd39cb2
Jesper Ryom <jesper@ryom.me>
2020-05-29 08:50:32 +0200

    feat: add useLocalStorage hook

---
+++ src/hooks/use-local-storage.js
@@ -0,0 +1,27 @@
+// https://usehooks.com/useLocalStorage/
+
+import { useState } from 'react';
+
+export default (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : undefined;
+    } catch (error) {
+      console.error(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value) => {
+    try {
+      const valToStore = value instanceof Function ? value(storedValue) : value;
+      setStoredValue(valToStore);
+      window.localStorage.setItem(key, JSON.stringify(valToStore));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return [storedValue, setValue];
+};
