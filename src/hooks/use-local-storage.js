99c61fa97700ec38ca517e2924272f2bd735cb7f
Jesper Ryom <jesper@ryom.me>
2020-06-03 22:31:11 +0200

    refactor: return undefined on SSR

--- src/hooks/use-local-storage.js
+++ src/hooks/use-local-storage.js
@@ -3,25 +3,31 @@
 import { useState } from 'react';
 
 export default (key, initialValue) => {
-  const [storedValue, setStoredValue] = useState(() => {
-    try {
-      const item = window.localStorage.getItem(key);
-      return item ? JSON.parse(item) : initialValue;
-    } catch (error) {
-      console.error(error);
-      return initialValue;
-    }
-  });
+  if (typeof window !== 'undefined') {
+    const [storedValue, setStoredValue] = useState(() => {
+      try {
+        const item = window.localStorage.getItem(key);
+        return item ? JSON.parse(item) : initialValue;
+      } catch (error) {
+        console.error(error);
+        return initialValue;
+      }
+    });
 
-  const setValue = (value) => {
-    try {
-      const valToStore = value instanceof Function ? value(storedValue) : value;
-      setStoredValue(valToStore);
-      window.localStorage.setItem(key, JSON.stringify(valToStore));
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    const setValue = (value) => {
+      try {
+        setStoredValue(value);
+        if (typeof value === 'undefined') {
+          window.localStorage.removeItem(key);
+        } else {
+          window.localStorage.setItem(key, JSON.stringify(value));
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-  return [storedValue, setValue];
+    return [storedValue, setValue];
+  }
+  return [undefined, () => {}];
 };
