46400dcea996aa9a37ab872a82d3b55d5565af57
Jesper Ryom <jesper@ryom.me>
2020-06-03 22:32:38 +0200

    feat: add settings to navigation

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -2,7 +2,9 @@ import css from '@styled-system/css';
 import { useRouter } from 'next/router';
 import styled from 'styled-components';
 
+import { SunIcon } from '@/components';
 import { PAGES } from '@/constants';
+import { useDidMount } from '@/hooks';
 import { Box, Link, Text } from '@/primitives';
 
 const Li = styled.li`
@@ -32,22 +34,78 @@ const Ol = styled.ol`
   })}
 `;
 
-export default () => {
+const Navigation = ({ setDarkMode, setSerif, isDarkMode, isSerif }) => {
   const { asPath } = useRouter();
 
+  const didMount = useDidMount();
+
   return (
-    <Box bg="accent" maxWidth={8} mx="auto" px={[2, 4]} py={[3, 4]}>
+    <Box
+      alignItems="center"
+      bg="var(--accent)"
+      display="flex"
+      justifyContent="space-between"
+      maxWidth={8}
+      mx="auto"
+      px={[2, 4]}
+      py={[3, 4]}
+    >
       <Ol>
         {Object.keys(PAGES).map((key) => (
           <Li key={PAGES[key]} active={asPath.includes(key.toLowerCase())}>
             <Link as={`/${key.toLowerCase()}`} href="/[page]">
-              <Text opacity="1" variant="m">
-                {PAGES[key]}
-              </Text>
+              <Text variant="m">{PAGES[key]}</Text>
             </Link>
           </Li>
         ))}
       </Ol>
+      <Box alignItems="center" display="flex">
+        <Box
+          alignItems="center"
+          as="button"
+          css={`
+            svg {
+              height: 100%;
+            }
+          `}
+          display="flex"
+          justifyContent="center"
+          marginRight={1}
+          onClick={() => setDarkMode(!isDarkMode)}
+          padding={[0, 1]}
+          pointer
+          size={['1.5rem', '2rem']}
+          type="button"
+        >
+          <SunIcon />
+        </Box>
+        <Box
+          alignItems="center"
+          as="button"
+          display="flex"
+          justifyContent="center"
+          onClick={() => setSerif(!isSerif)}
+          padding={[0, 1]}
+          pointer
+          size={['1.5rem', '2rem']}
+          type="button"
+        >
+          {didMount && (
+            <Text bold family={isSerif ? 'sans' : 'serif'} variant="s">
+              A
+            </Text>
+          )}
+        </Box>
+      </Box>
     </Box>
   );
 };
+
+Navigation.propTypes = {
+  setDarkMode: PropTypes.func.isRequired,
+  setSerif: PropTypes.func.isRequired,
+  isDarkMode: PropTypes.bool.isRequired,
+  isSerif: PropTypes.bool.isRequired,
+};
+
+export default Navigation;
